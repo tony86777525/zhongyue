73109fdc57af594bcd9a36abafa2ecc6c83d67f4
DC130 <tonyxu@core-tech.tw>
2023-02-02 17:12:08 +0800

    fix

--- resources/views/web/layouts/gtm_body.blade.php
+++ resources/views/web/layouts/gtm_body.blade.php
@@ -1,3 +1,4 @@
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PS3P3VJ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T59JLL3"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
